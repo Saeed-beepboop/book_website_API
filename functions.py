c00a37c3e350bb642817ee4aebd695ef87b3f65e
Saeed Afghah <saeedafghah@gmail.com>
2024-07-26 02:32:24 -0400

    updated functions to utilise loggers

--- functions.py
+++ functions.py
@@ -1,11 +1,17 @@
-# Importing necessaries libraries
+# Importing necessary libraries
 import requests
-import csv
-import numpy as np
 import pandas as pd
-import ast
-import seaborn as sns
-import matplotlib.pyplot as plt
+import logging
+
+# Setting up logger
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger(__name__)
 
 def get_book_list(params):
     """
@@ -13,8 +19,14 @@ def get_book_list(params):
     The output is a list of dictionaries, each being one book from the website's
     result.
     """
-    response = requests.get('https://openlibrary.org/search.json', params=params).json()
-    return response['docs']
+    try:
+        response = requests.get('https://openlibrary.org/search.json', params=params)
+        response.raise_for_status()
+        book_list = response.json()
+    except requests.exceptions.RequestException as e:
+        logger.error("Problem fetching book list")
+        raise SystemExit(e)
+    return book_list['docs']
 
 def get_book_info(book_list:list, columns_of_interest:list):
     """
@@ -27,38 +39,15 @@ def get_book_info(book_list:list, columns_of_interest:list):
         book_dict = {}
         for column in columns_of_interest:
             # Since some entries may be missing in the columns of interest,
-            # I used "try-except" to avoid errors.
+            # I used "try-except" to handle exceptions.
             try:
                 book_dict[column] = book[column]
             except KeyError as e:
                 book_dict[column] = ''
+                logger.debug(f"Column of interest missing. Moving on...\n{str(e)}")
                 continue
         book_list_selected.append(book_dict)
     return book_list_selected
-
-def create_csv(file_path_name:str, cleanedup_list):
-    """
-    Function to receive a path to where to file is going to be created, including
-    its name, (string) and a list of books, list of dictionaries, (list) to
-    create a csv file.
-    The output is a csv file created in the designated location.
-    """
-    with open(f'{file_path_name}.csv', 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=cleanedup_list[0].keys())
-        writer.writeheader()
-        for book in cleanedup_list:
-            writer.writerow(book)
-        return book
-
-def read_csv(file_path_name):
-    """
-    Function to receive a path to where to file is saved (string) to read the
-    csv file.
-    The output is the csv file opened.
-    """
-    file = pd.read_csv(f'{file_path_name}.csv')
-    return file
-
 
 def make_df(book_list:list, fields:list):
     df = pd.DataFrame(book_list)
